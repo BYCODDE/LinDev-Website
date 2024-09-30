a6b582686a9bb91ae2ccbcb8ba50b2cf202c37b7
BYCODDE <anaraliev217@gmail.com>
2024-09-30 21:43:27 +0400

    style:question style fixed!

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,24 +5,23 @@ function Question() {
         ხშირად დასმული შეკითხვები
       </span>
       <div className="mt-[2rem] flex flex-col gap-[2rem] w-full h-full justify-center items-center">
-        {/* Question Container */}
         {[...Array(4)].map((_, index) => (
           <div
             key={index}
-            className="relative cursor-pointer max-w-[1062px] rounded-3xl w-full flex p-4"
+            className="relative max-w-[1062px] w-full p-4 bg-white rounded-3xl flex justify-between items-center"
           >
-            <div
-              className="absolute inset-0 rounded-3xl"
-              style={{
-                border: "4px solid transparent", // Set the border to transparent
-                borderImage: "linear-gradient(to right, #2662D6, #2F847F) 1", // Apply the gradient
-              }}
-            ></div>
-            <span className="flex-1 text-transparent bg-clip-text bg-gradient-to-r from-[#2662D6] to-[#2F847F]">
-              {/* Gradient text */}
+            <div className="absolute inset-0 bg-gradient-to-r from-[#2662D6] to-[#2F847F] rounded-3xl p-[1px]">
+              <div className="bg-white w-full h-full rounded-3xl"></div>
+            </div>
+
+            <span className="relative flex-1 z-10 text-transparent bg-clip-text bg-gradient-to-r from-[#2662D6] to-[#2F847F]">
               რა სტრატეგიებს იყენებთ ვებაპლიკაციის მუშაობის ოპტიმიზაციისთვის?
             </span>
-            <img src="question_svg.svg" alt="question_svg" />
+            <img
+              className="relative z-10"
+              src="question_svg.svg"
+              alt="question_svg"
+            />
           </div>
         ))}
       </div>
